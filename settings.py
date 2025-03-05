ce2afc44db4c0d970dff5e99484fd930508daaa0
1sector <undeleted@icloud.com>
2025-03-05 12:14:20 +0200

    Обновлены настройки Django для админки

--- settings.py
+++ settings.py
@@ -1,13 +1,21 @@
+import os
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
     'tests',
-    'corsheaders',  # Добавьте это
+    'corsheaders',
+    'rest_framework',
 ]
 
 ALLOWED_HOSTS = ['*']  # В продакшене лучше указать конкретный домен
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -15,7 +23,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',  # Добавьте это
 ]
 
 CORS_ALLOWED_ORIGINS = [
@@ -25,4 +32,13 @@ CORS_ALLOWED_ORIGINS = [
 
 CSRF_TRUSTED_ORIGINS = [
     "https://psyho.netlify.app",
-] 
+]
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+SECRET_KEY = 'your-secret-key'
+DEBUG = True  # в продакшене установите False 
--- urls.py
+++ urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # админка будет доступна по /admin/
     path('tests/', include('tests.urls')),  # URLs для приложения tests
-] 
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
